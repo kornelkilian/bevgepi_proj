440e89ae77c5250efcb26914495f1f55dd6dcffe
Kilian Kornel <kilianivan@gmail.com>
2023-12-12 15:48:39 +0100

    changes

--- myapp.py
+++ myapp.py
@@ -7,18 +7,20 @@ model = joblib.load('mymodel.pkl')
 # Streamlit app
 st.title('Spotify Song Mode Prediction')
 
+# Define the list of key options
+key_options = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
+
 # User input
-key_encoded = st.slider('Key Encoded', min_value=0, max_value=11, value=5)
+key_encoded_text = st.selectbox('Key Encoded', key_options, index=5)  # Default to 'F'
+key_encoded = key_options.index(key_encoded_text)  # Convert the selected key to numerical value
 danceability = st.slider('Danceability (%)', min_value=0, max_value=100, value=50)
 instrumentalness = st.slider('Instrumentalness (%)', min_value=0, max_value=100, value=50)
 acousticness = st.slider('Acousticness (%)', min_value=0, max_value=100, value=50)
 liveness = st.slider('Liveness (%)', min_value=0, max_value=100, value=50)
 
-
-
 # Make prediction
 if st.button('Predict'):
-    features = [[acousticness, danceability,key_encoded,instrumentalness,liveness]]  # Add other features
+    features = [[acousticness, danceability, key_encoded, instrumentalness, liveness]]
     prediction = model.predict(features)
     st.write('Prediction:', prediction)
 
