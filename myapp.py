9ebe69c0aaff64578e1de934a6ac830f16dfa263
Kilian Kornel <77408636+kornelkilian@users.noreply.github.com>
2023-12-13 13:48:12 +0100

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -2,7 +2,7 @@ import streamlit as st
 import joblib
 
 # Load the trained model
-model = joblib.load('decisiontree.pkl')
+model = joblib.load('mymodel.pkl')
 
 # Streamlit app
 st.title('Spotify Song Mode Prediction')
@@ -11,7 +11,6 @@ st.title('Spotify Song Mode Prediction')
 key_options = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
 
 # User input
-key_options = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
 key_encoded_text = st.selectbox('Key Encoded', key_options, index=5)  # Default to 'F'
 key_encoded = key_options.index(key_encoded_text)  # Convert the selected key to numerical value
 danceability = st.slider('Danceability (%)', min_value=0, max_value=100, value=50)
@@ -21,7 +20,7 @@ liveness = st.slider('Liveness (%)', min_value=0, max_value=100, value=50)
 
 # Make prediction
 if st.button('Predict'):
-    features = [[acousticness, danceability, key_encoded,danceability, instrumentalness, liveness]]
+    features = [[acousticness, danceability, key_encoded, instrumentalness, liveness]]
     prediction = model.predict(features)
     st.write('Prediction:', prediction)
 
